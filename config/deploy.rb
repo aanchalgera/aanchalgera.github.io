a7fe61d1655fa979e80f39370d3f5038e1d9f229
Ajinder Singh <ajinder@agilemedialab.in>
2015-08-17 17:33:50 +0530

    fixed deployment issues

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,12 +23,6 @@ set :copy_compression, :gz
 set :keep_releases, 2
 set :admin_runner, 'root'
 
-# s3_config = YAML::load(ERB.new(IO.read("/etc/s3.yml")).result)
-# set :s3_config, s3_config
-# set :aws_access_key_id, s3_config['access_key_id']
-# set :aws_secret_access_key, s3_config['secret_access_key']
-# set :tmpdir, '/tmp'
-
 ssh_options[:keys] = ENV['ssh_keys'] ? ENV['ssh_keys'] : %w(~/.ec2/agile_new.pem)
 set :revision, ENV['BUILD_NUMBER']
 
@@ -36,7 +30,6 @@ set :revision, ENV['BUILD_NUMBER']
 # TASKS
 # =============================================================================
 namespace :deploy do
-  before "deploy:update_code", :build_namespace
   after "deploy:update_code", :create_symlinks
   after "deploy:restart", "deploy:cleanup"
 
@@ -48,11 +41,6 @@ namespace :deploy do
   end
 end
 
-task :build_namespace, :once => true do
-  logger.info "revision #{revision}"
-  s3_config["package_prefix"] = "#{application}_#{stage}"
-end
-
 task :create_symlinks do
-  run "ln -s /var/www/cms-prototype #{release_path}/web/cms-prototype"
+  run "ln -s /var/www/cms-prototype #{release_path}"
 end
