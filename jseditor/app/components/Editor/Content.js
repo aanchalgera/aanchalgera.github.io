9a919f1c68a847a317ba82a66e329cd30e7fdc25
Arpit <rawat.arpit@gmail.com>
2015-08-18 13:42:20 +0000

    moved drag drop code to editor level

--- jseditor/app/components/Editor/Content.js
+++ jseditor/app/components/Editor/Content.js
@@ -3,9 +3,11 @@ import PropertyButton from './PropertyButton';
 
 class Content extends React.Component{
   componentDidMount() {
-    this.intializeEditor(this.props.index);
-    var currentRef = 'myInput' + this.props.dataId;
-    this.refs[currentRef].getDOMNode().focus();
+    if ('content' == this.props.type) {
+      this.intializeEditor(this.props.index);
+      var currentRef = 'myInput' + this.props.dataId;
+      this.refs[currentRef].getDOMNode().focus();
+    }
   }
   intializeEditor(editArea) {
     AlloyEditor.editable(editArea,{
@@ -24,17 +26,32 @@ class Content extends React.Component{
     });
   }
   render () {
+    var propertyButton = '';
+    var showBackgroundImageField = true;
+    if('content' == this.props.type) {
+      var field = <p
+        id={this.props.index}
+        key={this.props.data.id}
+        ref={'myInput' + Number(this.props.dataId)}
+        data-id={this.props.dataId} className="form-control-static"
+        onKeyDown={this.props.addNewTextArea.bind(this)}>
+        {this.props.data.text}
+      </p>;
+    } else if('image' == this.props.type) {
+      var field = <p key={this.props.data.id}>
+          <img src={this.props.data.url} height={this.props.data.height} width={this.props.data.width} />
+        </p>;
+      showBackgroundImageField = false;
+    }
     return (
       <span>
-        <p id={this.props.index} ref={'myInput' + Number(this.props.dataId)} data-id={this.props.dataId} className="form-control-static" onKeyDown={this.props.addNewTextArea.bind(this)}>
-          {this.props.text}
-        </p>
-        <PropertyButton />
+        {field}
+        <PropertyButton fieldClass={showBackgroundImageField ? '' : '.hidden'} />
       </span>
     )
   }
 }
 
-Content.propTypes = { text: React.PropTypes.string.isRequired };
+Content.propTypes = { type: React.PropTypes.string.isRequired };
 
 export default Content
--- jseditor/app/components/Editor/ContentList.js
+++ jseditor/app/components/Editor/ContentList.js
@@ -1,74 +1,29 @@
 import React from 'react';
 import Content from './Content';
 
-var map = [];
-var counter = 0;
-var placeholder = document.createElement("div");
-placeholder.className = "placeholder";
-
 class ContentList extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      data : this.props.fields
-    };
-  }
-  dragStart(e) {
-    this.dragged = e.currentTarget;
-    e.dataTransfer.effectAllowed = 'move';
-        
-    // Firefox requires calling dataTransfer.setData
-    // for the drag to properly work
-    e.dataTransfer.setData("text/html", e.currentTarget);
-  }        
-  dragEnd(e) {
-        
-    this.dragged.style.display = "block";
-    document.getElementById("myList").removeChild(placeholder);
-    
-    // Update state
-    var data = this.state.data;
-    var from = Number(this.dragged.dataset.id);
-    var to = Number(this.over.dataset.id);
-    if(from < to) to--;
-    data.splice(to, 0, data.splice(from, 1)[0]);
-    this.setState({data: data});
-  }
-  dragOver(e) {
-    e.preventDefault();
-    this.dragged.style.display = "none";
-    if(e.target.className == "placeholder")
-	 return;
-    this.over = e.target;
-    var parentDiv = this.parentDiv(e.target);
-    if (parentDiv.parentNode.id != 'myList') return;
-    parentDiv.parentNode.insertBefore(placeholder, parentDiv);
-  }
-  parentDiv(el) {
-    while (el && el.parentNode) {
-      el = el.parentNode;
-      if (el.tagName && el.tagName.toLowerCase() == 'div') {
-        return el;
-      }
-    }
-}
-
   render(){
-    var fields = this.props.fields.map((text, i) => {
+    var fields = this.props.fields.map((field, i) => {
       var index = "text-area" + i;
       return (
         <div className="container-ul-iiner" data-id={i}
             key={i}
             draggable="true"
-            onDragEnd={this.dragEnd.bind(this)}
-            onDragStart={this.dragStart.bind(this)}>
-          <Content key={i} dataId={i} text={text} index={index} addNewTextArea={this.props.addNewTextArea.bind(this)} />
+            onDragEnd={this.props.dragEnd.bind(this)}
+            onDragStart={this.props.dragStart.bind(this)}>
+          <Content
+            key={i}
+            dataId={i}
+            type={field.type}
+            data={field}
+            index={index}
+            addNewTextArea={this.props.addNewTextArea.bind(this)} />
         </div>
       )
     });
     return (
       <div className="col-sm-6 content-area container-ul" id="main-container">
-	<ul id ="myList" onDragOver={this.dragOver.bind(this)}>{fields}</ul>
+        <ul id ="myList" onDragOver={this.props.dragOver.bind(this)}>{fields}</ul>
       </div>
     )
   }
