adb78e70fa458ab8f0463bd2ce26e90919b30901
Aanchal <aanchal.gera@gmail.com>
2015-08-20 15:53:30 +0530

    fix issue of image drag and drop

--- jseditor/app/components/Editor/ContentList.js
+++ jseditor/app/components/Editor/ContentList.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import Content from './Content';
 
-var map = [];
-var counter = 0;
-
 class ContentList extends React.Component{
   render(){
-    var fields = this.props.fields.map((text, i) => {
-      var index = "text-area" + i;
+    var fields = this.props.fields.map((field, i) => {
+      var index = "text-area" + field.id;
       return (
-        <div className="container-ul-iiner">
-          <Content key={i} dataId={i} text={text} index={index} addNewTextArea={this.props.addNewTextArea.bind(this)} />
+        <div className="container-ul-iiner" data-id={field.id}
+            draggable="true"
+            onDragEnd={this.props.dragEnd.bind(this)}
+            onDragStart={this.props.dragStart.bind(this)}>
+          <Content
+            key={index}
+            dataId={field.id}
+            type={field.type}
+            data={field}
+            index={index}
+            addNewTextArea={this.props.addNewTextArea.bind(this)} />
         </div>
       )
     });
     return (
       <div className="col-sm-6 content-area container-ul" id="main-container">
-        {fields}
+        <ul id ="myList" onDragOver={this.props.dragOver.bind(this)}>{fields}</ul>
       </div>
     )
   }
