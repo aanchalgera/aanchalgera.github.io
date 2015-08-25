33f898c5a28416c8b94c55fa0d6f238b2589b6fb
Arpit <rawat.arpit@gmail.com>
2015-08-25 08:12:42 +0000

    delete image options in cloudinary

--- jseditor/app/components/Editor/Thumbnail.js
+++ jseditor/app/components/Editor/Thumbnail.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
 class Thumbnail extends React.Component {
+  removeImage (ev) {
+    ev.preventDefault();
+    ev.currentTarget.parentElement.style.display = 'none';
+  }
   render () {
     return (
-      <img
-        alt="some title"
-        src={this.props.data.thumbnail_url}
-        draggable="true"
-        data-src={this.props.data.url}
-        data-height={this.props.data.height}
-        data-width={this.props.data.width}
-        onDragStart={this.props.dragImageStart.bind(this)}
-        onDragEnd={this.props.dragImageEnd.bind(this)}
-      />
+      <li>
+        <a href="#" className="glyphicon glyphicon-remove" onClick={this.removeImage.bind(this)} />
+        <img
+          alt={this.props.data.original_filename}
+          src={this.props.data.thumbnail_url}
+          draggable="true"
+          data-src={this.props.data.url}
+          data-height={this.props.data.height}
+          data-width={this.props.data.width}
+          onDragStart={this.props.dragImageStart.bind(this)}
+          onDragEnd={this.props.dragImageEnd.bind(this)}
+        />
+      </li>
     )
   }
 }
