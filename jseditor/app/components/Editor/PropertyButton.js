2dc4d37370f194f577d07ce325022df6fd80a74e
Aanchal <aanchal.gera@gmail.com>
2015-08-27 18:51:10 +0530

    show errors if columns are not left/right justified properly

--- jseditor/app/components/Editor/PropertyButton.js
+++ jseditor/app/components/Editor/PropertyButton.js
@@ -9,12 +9,10 @@ class PropertyButton extends React.Component {
         rightAlignActive = 'active';
     }
     return (
-	<ul className="nav nav-pills nav-pills2">
-      	  <div className="btn-group">
-   	    <button onClick={this.props.addClassToResource} aria-label="Left Align" className={"btn btn-default" + leftAlignActive} type="button"><span data-alignment="section-align-left" aria-hidden="true" className="glyphicon glyphicon-align-left"></span></button>
-   	    <button onClick={this.props.addClassToResource} aria-label="Right Align" className={"btn btn-default "+ rightAlignActive} type="button"><span data-alignment="section-align-right" aria-hidden="true" className="glyphicon glyphicon-align-right"></span></button>
+	<ul className="nav nav-pills nav-pills2 btn-group">
+   	    <button data-alignment="section-align-left" onClick={this.props.addClassToResource} aria-label="Left Align" className={"btn btn-default" + leftAlignActive} type="button"><span data-alignment="section-align-left" aria-hidden="true" className="glyphicon glyphicon-align-left"></span></button>
+   	    <button data-alignment="section-align-right" onClick={this.props.addClassToResource} aria-label="Right Align" className={"btn btn-default "+ rightAlignActive} type="button"><span data-alignment="section-align-right" aria-hidden="true" className="glyphicon glyphicon-align-right"></span></button>
    	    <button aria-label="Delete" className="btn btn-default" type="button"><span aria-hidden="true" className="glyphicon glyphicon-trash"></span></button>
-      	  </div>
       	</ul>
     )
   }
