903bc9ca6b80ac6a0dd36ea4e8cf6c3bdcb90f32
Aanchal <aanchal.gera@gmail.com>
2015-08-31 19:02:37 +0530

    toggle box

--- jseditor/app/components/Editor/PropertyButton.js
+++ jseditor/app/components/Editor/PropertyButton.js
@@ -1,19 +1,39 @@
 import React from 'react';
 
 class PropertyButton extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {box : 'open'};
+  }
+ 
+  expandBox() {
+    this.setState({box:'open'});
+  }
+  closeBox() {
+    this.setState({box:'close'});
+  }
+
   render() {
     var leftAlignActive = "", rightAlignActive = "";
+    var closeStyle = '',expandStyle='';
     if (this.props.align == 'section-align-left') {
 	leftAlignActive = 'active';
     } else if (this.props.align == 'section-align-right') {
         rightAlignActive = 'active';
     }
+    if (this.state.box == 'open') {
+      closeStyle = {display : 'none'}
+      expandStyle = {display : 'block'}
+    } else { 
+      closeStyle = {display : 'block'}
+      expandStyle = {display : 'none'}
+    }
     return (
       <ul>
-	<ul className="nav-pills2"><li>
-          <button type="button" className="btn btn-default btn-properties" style={{display:'none'}}>Properties</button>
+        <ul className="nav-pills2"><li>
+          <button type="button" onClick={this.expandBox.bind(this)} className="btn btn-default btn-properties" style={closeStyle}>Properties</button>
        	</li></ul>
-	<ul className="nav nav-pills nav-pills2 js-properties-container">
+	<ul className="nav nav-pills nav-pills2 js-properties-container" style={expandStyle}>
           <li>
             <span className="btn-group" role="group" aria-label="...">
               <span className="btn-group dropdown" role="group">
@@ -33,7 +53,7 @@ class PropertyButton extends React.Component {
   	      <button data-align="section-align-left" onClick={this.props.addClassToResource} type="button" className={"btn btn-default "+leftAlignActive}>Column Left</button>
   	      <button data-align="section-align-right" onClick={this.props.addClassToResource} type="button" className={"btn btn-default " + rightAlignActive}>Column Right</button> 
   	      <button type="button" className="btn btn-default">Delete</button>
-  	      <button type="button" className="btn btn-default btn-proeprties-close">X</button>
+  	      <button onClick={this.closeBox.bind(this)} type="button" className="btn btn-default btn-proeprties-close">X</button>
 	    </span>
           </li>
         </ul>
