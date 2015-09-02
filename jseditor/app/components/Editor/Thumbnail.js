9623165cf080a3592163c1dca8e683f1514cb3b2
Arpit <rawat.arpit@gmail.com>
2015-09-02 13:30:19 +0000

    minified bundle js

--- jseditor/app/components/Editor/Thumbnail.js
+++ jseditor/app/components/Editor/Thumbnail.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from 'react/addons';
 
-class Thumbnail extends React.Component {
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+export default class Thumbnail extends React.Component {
   render () {
     return (
       <li>
-        <img
-          src={this.props.data.thumbnail_url}
-          draggable="false"
-        />
+        <ReactCSSTransitionGroup transitionName="carousel">
+          <img
+            src={this.props.data.thumbnail_url}
+            draggable="false"
+            alt=""
+          />
+        </ReactCSSTransitionGroup>
       </li>
     )
   }
 }
-
-export default Thumbnail;
