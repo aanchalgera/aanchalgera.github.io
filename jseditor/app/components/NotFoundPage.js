85be14749681985665cab0903534a55fd1dcb164
aanchalgera <aanchal.gera@gmail.com>
2015-09-10 15:19:23 +0530

    Update NotFoundPage.js

--- jseditor/app/components/NotFoundPage.js
+++ jseditor/app/components/NotFoundPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 class NotFoundPage extends React.Component {
   render () {
     return (
-      <h2>D'oh!!, Page Not Found.. Come back later.</h2>
+      <h2>Oh!!, Page Not Found.. Come back later.</h2>
     )
   }
 }
