7775f903642904f412bb0ee6739fb0c0f41f9a96
Aanchal <aanchal.gera@gmail.com>
2015-09-16 18:27:54 +0530

    fix text area

--- jseditor/app/components/Editor/MoreOptions.js
+++ jseditor/app/components/Editor/MoreOptions.js
@@ -5,7 +5,7 @@ class MoreOptions extends React.Component {
     return (
       <div className="add-more-options"><a className="btn-circle glyphicon glyphicon-plus" title="Add image, video, slider, new section" />
         <span className="add-options">
-          <a href="#" className="btn-circle glyphicon" onClick={this.props.addTextArea.bind(this,this.props.dataId)} title="Add new section">T</a>
+          <a href="#" className="btn-circle glyphicon" onClick={this.props.addTextArea.bind(this,this.props.dataId,"content")} title="Add new section">T</a>
           <a href="#" className="btn-circle glyphicon glyphicon-camera" onClick={this.props.openResourcePanel.bind(this,'image', this.props.dataId, 'hidden')} title="Add Image" />
           <a href="#" className="btn-circle glyphicon glyphicon-picture" onClick={this.props.openResourcePanel.bind(this,'image', this.props.dataId, 'checkbox')} title="Add Gallery" />
           <a href="#" className="btn-circle glyphicon glyphicon-file" onClick={this.props.addTextArea.bind(this,this.props.dataId,"summary")} title="Add Summary"/>
