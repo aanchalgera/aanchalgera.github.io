92ea4d5a1b0ebe77d9abb35c10cfa7093d33e1fd
Arpit <rawat.arpit@gmail.com>
2015-10-15 11:04:13 +0000

    editor page changes for publish

--- jseditor/app/components/Editor/Editor.js
+++ jseditor/app/components/Editor/Editor.js
@@ -40,7 +40,8 @@ class Editor extends React.Component{
               id : data.id,
               fields: data.sections != undefined ? data.sections : [],
               value: data.title,
-              maxId: data.maxId
+              maxId: data.maxId,
+              publishData: data.publishData != undefined ? data.publishData : {'publishRegion' : ['ES','US','MX','PE','ROW']}
             });
           }
         }
@@ -247,8 +248,7 @@ class Editor extends React.Component{
       "title" : this.state.value,
       "sections" : this.state.fields,
       "maxId" : this.state.maxId,
-      "status" : 'future',
-      "date" : '2015-10-20 08:31:25'
+      "publishData" : this.state.publishData
     };
     self = this;
     this.props.base.post(
@@ -463,11 +463,12 @@ class Editor extends React.Component{
         <div className="preview-nav">
           <a className="btn btn-primary" href="#" onClick={this.openPreviewPanel.bind(this)}>Preview</a>
           <Link className="btn btn-primary" to="/">List Page</Link>
+          <Link className="btn btn-primary" to={"/publish/"+this.state.id}>Publicar</Link>
         </div>
         <br /><br />
         {errorField}
         {successField}
-        <form id="editor-form" onClick={this.saveData.bind(this)}>
+        <form id="editor-form">
           <div className="form-group">
             <label className="col-sm-12 control-label">Title</label>
             <PostTitle value={this.state.value} handleChange={this.handleChange.bind(this)} handleBlur={this.handleBlur.bind(this)}/>
@@ -491,7 +492,6 @@ class Editor extends React.Component{
               ungroupSections={this.ungroupSections.bind(this)}
             />
           </div>
-          <div className="submit-area"><button className="btn btn-primary">Submit</button></div>
         </form>
         <CloudinaryUploader
           cloudName='realarpit'
