2b6db15a3a4979c786d81022fd9b9262d7c4c686
Ajinder Singh <ajinder@agilemedialab.in>
2015-11-14 02:16:21 +0530

    done changes for meta

--- request.js
+++ request.js
@@ -1,16 +1,19 @@
 var request = require("request");
 var fs = require('fs');
 
-var testJson = fs.readFileSync('./templates/allcases.json', 'utf8');
+var testJson = fs.readFileSync('./templates/test.json', 'utf8');
 
 request({
-  uri: "http://localhost:3000/parse",
+  uri: "http://localhost:81/parse",
   method: "POST",
-  json: testJson
+  json: testJson,
+  headers: {
+      "content-type": "application/json",
+  }
 }, function(error, response, body) {
-	if (null === error) {
-  		// console.log(body);
-	} else {
-		console.log(error);
-	}
+    if (null === error) {
+        console.log(body);
+    } else {
+        console.log(error);
+    }
 });
