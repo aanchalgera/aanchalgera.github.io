d734bd2d3a9ab15895560a4f0383f976acce6c46
Ajinder Singh <ajinder@agilemedialab.in>
2015-11-28 01:03:20 +0530

    inital commit

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express')
 ,   bodyParser = require('body-parser')
 ,   app = express()
 ,   parserRoutes = require(__dirname + '/jsonParser/routes')
+,   configRoutes = require(__dirname + '/config/routes')
 ;
 
 app.use(bodyParser.json({ type: 'text/plain' }));
 app.use('/parse', parserRoutes);
+app.use('/config', configRoutes);
 
 app.listen(81);
