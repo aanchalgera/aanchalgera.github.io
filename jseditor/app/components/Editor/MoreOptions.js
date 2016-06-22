0e0c092afd10a744c5a3a37e14dc2ba98ebd8b0c
Sarbjit Singh <sarbjit212@gmail.com>
2016-06-22 10:03:35 +0000

    UI Improvements

--- jseditor/app/components/Editor/MoreOptions.js
+++ jseditor/app/components/Editor/MoreOptions.js
@@ -9,10 +9,10 @@ class MoreOptions extends React.Component {
         <button type="button" className="btn glyphicon glyphicon-plus" title="Add image, video, slider, new section" ></button>
         <span className="add-options">
           <button type="button" className="btn glyphicon glyphicon-text" onClick={this.props.addTextArea.bind(this,this.props.dataId,"content")} title="Add new section">T</button>
-          <button type="button" className="btn glyphicon glyphicon-camera" onClick={this.props.openResourcePanel.bind(this,'image', this.props.dataId, '', false)} title="Add Image" ></button>
+          <button type="button" className="btn glyphicon glyphicon-picture" onClick={this.props.openResourcePanel.bind(this,'image', this.props.dataId, '', false)} title="Add Image" ></button>
           <button type="button" className="btn glyphicon glyphicon-file" onClick={this.props.addTextArea.bind(this,this.props.dataId,"summary")} title="Add Summary"></button>
           <button type="button" className="btn glyphicon glyphicon-facetime-video" onClick={this.props.addVideo.bind(this,this.props.dataId)} title="Add Video"></button>
-          <button type="button" className="btn glyphicon glyphicon-picture" onClick={this.props.openResourcePanel.bind(this,'image', this.props.dataId, 'gallery', false)} title="Add Gallery" ></button>
+          <button type="button" className="btn glyphicon glyphicon-camera" onClick={this.props.openResourcePanel.bind(this,'image', this.props.dataId, 'gallery', false)} title="Add Gallery" ></button>
           <button type="button" className="btn glyphicon glyphicon-console" onClick={this.props.addTextArea.bind(this,this.props.dataId,"richContent")} title="Add rich content snippet"></button>
           <button type="button" className="btn glyphicon glyphicon-tasks" onClick={this.props.openResourcePanel.bind(this,'image', this.props.dataId, 'slider', false)} title="Add Slider" ></button>
           {this.props.show2column ? twoColumnButton: ''}
