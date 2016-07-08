d256c7bfdd12569671348f9d0e9edf96cbe2fc21
Aanchal Gera <aanchal.gera@gmail.com>
2016-07-08 05:30:44 +0000

    revert

--- jseditor/webpack.config.js
+++ jseditor/webpack.config.js
@@ -54,6 +54,11 @@ module.exports = {
         include: /\.json$/,
         loader: 'json-loader'
       },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
     ]
   },
   resolve: {
