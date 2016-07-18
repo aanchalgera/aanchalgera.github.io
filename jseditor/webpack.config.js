b74d9226886b018474031fa2c8fc4667801fb8b1
Arpit <rawat.arpit@gmail.com>
2016-07-18 12:46:45 +0000

    added api keys for firebase

--- jseditor/webpack.config.js
+++ jseditor/webpack.config.js
@@ -3,6 +3,7 @@ var NODE_ENV = process.env.NODE_ENV || 'development';
 
 var configs = {
   production: {
+    apiKey: 'YJsHk9nHLb0MNnwFCqCFIDH6CCCHUvkOxP9FhX06',
     host: 'http://admin.weblogssl.com',
     blogUrl: 'http://www.xataka.com/',
     firebaseUrl: 'https://brilliant-heat-3614.firebaseio.com/',
@@ -11,6 +12,7 @@ var configs = {
     timezone: 'Europe/Madrid'
   },
   testing: {
+    apiKey: 'JCIDuff5nTLMU6zfXXtcVjIzmvkvgruL573ldNdC',
     host: 'http://test.admin.weblogssl.com',
     blogUrl: 'http://testing.xataka.com/',
     firebaseUrl: 'https://dazzling-torch-3017.firebaseio.com/',
