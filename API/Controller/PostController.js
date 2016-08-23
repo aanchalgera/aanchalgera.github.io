ff65e88cbd6e1789a63d0f2148ad6b0bcc19057d
Aanchal Gera <aanchal.gera@gmail.com>
2016-08-23 10:47:06 +0000

    update in post_list

--- API/Controller/PostController.js
+++ API/Controller/PostController.js
@@ -14,13 +14,18 @@ export default class Controller {
   }
 
   static updatePost(req, res, next) {
+    console.log(req.params.id);
     firebaseApp.database().ref('posts/'+ req.params.id).once('value', function(snap) {
       if (snap.val()) {
-        var user_status = snap.val().blogname+'_'+ snap.val().user_id + '_' + req.query.status;
-        var blog_status = snap.val().blogname+'_'+req.query.status;
+        var user_status = snap.val().blogName+'_'+ snap.val().user_id + '_' + req.query.status;
+        var blog_status = snap.val().blogName+'_'+req.query.status;
         firebaseApp.database().ref('posts/'+ req.params.id).update({'status': req.query.status,'user_status': user_status,'blog_status':blog_status});
+        firebaseApp.database().ref('posts_list/'+ req.params.id).update({'status': req.query.status,'user_status': user_status,'blog_status':blog_status});
+        res.send("done");
+      }else{
+        res.send("No such post");
       }
     });
-    res.send("done");
   }
 }
+
