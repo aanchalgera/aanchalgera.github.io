4d80a413cf103e1110a9fd35add939c02696f3fd
pradeep013 <pradeep@agilemedialab.in>
2016-08-23 07:50:40 +0000

    Minor changes

--- API/server.js
+++ API/server.js
@@ -10,6 +10,6 @@ let routes = require('./Router');
 
 app.use('/api', routes);
 
-app.listen(3000, () => 
-  console.log('LFE listening on port 3000!')
+app.listen(81,"0.0.0.0", () => 
+  console.log('LFE listening on port 81!')
 );
