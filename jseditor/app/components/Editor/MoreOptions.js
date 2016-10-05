63b0787707056a81e42f9abee3140e134947536d
Sarbjit Singh <sarbjit212@gmail.com>
2016-10-05 16:36:19 +0530

    Revert "Table: Temporary disable"

--- jseditor/app/components/Editor/MoreOptions.js
+++ jseditor/app/components/Editor/MoreOptions.js
@@ -4,6 +4,7 @@ class MoreOptions extends React.Component {
   render() {
     const twoColumnButton = <button type="button" className="btn" onClick={() => this.props.groupSections(this.props.dataId, 2)} title="2 Columns"> 2 Columns</button>;
     const threeColumnButton = <button type="button" className="btn" onClick={() => this.props.groupSections(this.props.dataId, 3)} title="3 Columns"> 3 Columns</button>;
+    const tableButton = <button type="button" className="btn fa fa-table" onClick={() => this.props.addTable(this.props.dataId)} title='Add Tables'></button>;
     return (
       <div className="add-more-options">
         <button type="button" className="btn glyphicon glyphicon-plus" title="Add image, video, slider, new section" ></button>
@@ -17,6 +18,7 @@ class MoreOptions extends React.Component {
           <button type="button" className="btn glyphicon glyphicon-tasks" onClick={() => this.props.openResourcePanel('image', this.props.dataId, 'slider', false)} title="Add Slider" ></button>
           <button type="button" className="btn" onClick={() => this.props.addResource({type: 'giphy', currentIndex: this.props.dataId})} title="Add Giphy content">GIF</button>
           <button type="button" className="btn glyphicon glyphicon-stats" onClick={() => this.props.addResource({type: 'infogram', currentIndex: this.props.dataId})} title="Add Infogram/Datawrapper content"></button>
+          {this.props.showTableButton === false ? null : tableButton}
           {this.props.show2column ? twoColumnButton : ''}
           {this.props.show3column ? threeColumnButton : ''}
         </span>
