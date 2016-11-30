6ffea8b1047585965bfa10dec10611d3e20e91cc
vishwasg <vishwas@agilemedialab.in>
2016-11-30 13:13:57 +0000

    Use global state for twitter text and remove extra listener https://github.com/agilemedialab/cms-prototype/pull/458#discussion-diff-90198121R33

--- jseditor/app/components/Editor/Metadata/Social.js
+++ jseditor/app/components/Editor/Metadata/Social.js
@@ -3,19 +3,6 @@ import React from 'react';
 const TWITTER_CHAR_LIMIT = 116;
 
 class Social extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      twitterText: ''
-    };
-    this.handleTwitterKeyUp = this.handleTwitterKeyUp.bind(this);
-  }
-
-  handleTwitterKeyUp (event) {
-    this.setState({twitterText: event.target.value});
-  }
-
   render () {
     return (
       <div className="modules module-home expandmodule expandmodule9">
@@ -26,13 +13,12 @@ class Social extends React.Component {
         <div ref={(c) => this._articleMetaPanel = c}>
           <div className="form-group">
             <label>Text for twitter</label>
-            <span className="pull-right">{TWITTER_CHAR_LIMIT - this.state.twitterText.length}</span>
+            <span className="pull-right">{TWITTER_CHAR_LIMIT - this.props.social.twitter.length}</span>
             <textarea
               defaultValue={this.props.social.twitter}
               className="form-control"
-              maxLength="116"
-              onBlur={this.props.updateSocialTwitterText}
-              onKeyUp={this.handleTwitterKeyUp}
+              maxLength={TWITTER_CHAR_LIMIT}
+              onKeyUp={this.props.updateSocialTwitterText}
             />
           </div>
           <div className="form-group">
