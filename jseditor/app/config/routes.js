d27689aafb78efc7b349d0fca3aa18c0912c5c86
Vaibhav Sood <vaibhav@agilemedialab.in>
2017-07-03 07:54:05 +0000

    Started using material in publicar

--- jseditor/app/config/routes.js
+++ jseditor/app/config/routes.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import Main from '../components/Main';
-import Publish from '../containers/Publish';
-import { Switch, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
-export default () => (
-  <Switch>
-    <Route path="/publicar/:postname" component={Publish}/>
-    <Route path="/" component={Main}/>
-  </Switch>
-);
+export default () => <Route path="/" component={Main}/>;
