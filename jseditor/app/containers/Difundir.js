d4a212ebfe15894785ab7b1b17f8cab15ddd4409
nishu <nishubindal89@gmail.com>
2017-07-04 17:46:39 +0530

    Changes to save data to firebase

--- jseditor/app/containers/Difundir.js
+++ jseditor/app/containers/Difundir.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Snackbar from 'material-ui/Snackbar';
 
 import RepostBlogsOptions from '../components/Editor/DraftJSEditor/RepostBlogsOptions';
 
@@ -12,8 +13,82 @@ class Difundir extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: '',
       postRepostBlogNames: [],
+      postDate: '',
+      publishRegion: [],
+      blogName: '',
+      snackbarOpen: false
     };
+  }
+
+  componentDidMount() {
+    this.init();
+  }
+
+  componentWillMount() {
+    this.checkUser();
+  }
+
+  checkUser() {
+    const {
+      match: { params: { postname } },
+      location: { search },
+      history
+    } = this.props;
+
+    const query = new URLSearchParams(search);
+
+    this.userId = query.get('userid');
+    this.blogName = query.get('blog');
+    let regEx = /\D/;
+    if (regEx.test(this.userId)) {
+      history.push('/invalidUser');
+    }
+    this.postname = postname;
+  }
+
+  init() {
+    const { history } = this.props;
+
+    if (this.blogName == undefined) {
+      history.replace('/invalidBlog');
+    } else {
+      this.props.base.fetch('config', {
+        context: this,
+        asArray: true,
+        queries: {
+          orderByChild: 'site_name',
+          equalTo: this.blogName
+        },
+        then(data) {
+          if (data[0] != null) {
+            this.setState({
+              blogName: this.blogName,
+              blogUrl: data[0].site_url
+            });
+          } else {
+            history.replace('/invalidBlog');
+          }
+        }
+      });
+    }
+
+    if (this.postname != undefined) {
+      this.props.base.fetch('posts/' + this.postname, {
+        context: this,
+        then(data) {
+          if (data.hasOwnProperty('id')) {
+            this.setState({
+              id: data.id,
+              postRepostBlogNames: data.publishData.postRepostBlogNames || [],
+              publishRegion: data.publishData.publishRegion || '',
+              postDate: data.publishData.postDate || '',
+            });
+          }
+        }
+      });
+    }
   }
 
   setRepostBlogs = (blogName, isChecked) => {
@@ -31,12 +106,42 @@ class Difundir extends React.Component {
     this.setState({postRepostBlogNames});
   }
 
+  submitRepostedBlogs = () => {
+    const publishData = {
+      postRepostBlogNames: this.state.postRepostBlogNames,
+      publishRegion: this.state.publishRegion,
+      postDate: this.state.postDate,
+    };
+
+    this.props.base.update(
+      'posts/' + this.state.id,
+      {
+        data: {publishData},
+        then: () => {
+          this.setState({snackbarOpen: true});
+        }
+      }
+    );
+  }
+
+  handleSnackbarClose = () => {
+    this.setState({snackbarOpen: false});
+  }
+
   render() {
     return (
       <div style={styles.bodyContent}>
+        <Snackbar
+          open={this.state.snackbarOpen}
+          message="Data Saved Successfully"
+          autoHideDuration={5000}
+          onRequestClose={this.handleSnackbarClose}
+        />
         <RepostBlogsOptions 
           setRepostBlogs={this.setRepostBlogs}
           repostBlogs={this.state.postRepostBlogNames}
+          blogName={this.state.blogName}
+          submitRepostedBlogs={this.submitRepostedBlogs}
         />
       </div>
     );
