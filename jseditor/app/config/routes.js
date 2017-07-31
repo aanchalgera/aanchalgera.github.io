381f005662f52e94957dfecffb9ffcab59a2a662
Aanchal <aanchal.gera@gmail.com>
2017-07-31 10:39:27 +0000

    create_react_app

--- jseditor/app/config/routes.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Main from '../components/Main';
-import { Route } from 'react-router-dom';
-
-export default () => <Route path="/" component={Main}/>;
--- jseditor/app/constants/ActionTypes.js
+++
@@ -1,1 +0,0 @@
-export const RECEIVE_CONFIGS = 'RECEIVE_CONFIGS';
