a416712937e77bbfb89d7269a00013dd985086c4
Hemant Kumar <hemant@agilemedialab.in>
2017-08-11 05:24:40 +0000

    minor changes

--- jseditor/src/components/Editor/Publish/lib/publishHelpers.js
+++ jseditor/src/components/Editor/Publish/lib/publishHelpers.js
@@ -2,7 +2,7 @@
 type User = {id: number, display_name: string}
 
 export const findById = <T: {id: number}>(id :number, list : Array<T>) : T|void =>
-list.find((item: T) => item.id == id);
+list.find((item: T) => item.id === id);
 
 export const findByName = (name :string, list : Array<User>) => list.find(item => item.display_name == name);
 
