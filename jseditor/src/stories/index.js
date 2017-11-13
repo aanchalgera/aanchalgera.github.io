888bf6f851def79e64819430afcae1302a4197e4
Aanchal Sharma <aanchal1103@gmail.com>
2017-11-13 11:54:32 +0000

    storybook changes

--- jseditor/src/stories/index.js
+++ jseditor/src/stories/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
-import { storiesOf } from '@storybook/react';
+import { storiesOf, addDecorator } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { MuiThemeProvider } from 'material-ui/styles';
+import { customTheme } from '../containers/styles/customTheme';
 
 import ImageAltTextPopover from '../components/Editor/Escribir/ImageAltTextPopover';
 import ImagePanel from '../components/Editor/Escribir/ImagePanel';
 
-import { MuiThemeProvider } from 'material-ui/styles';
-import { customTheme } from '../containers/styles/customTheme';
+const muiDecorator = storyFn => (
+  <MuiThemeProvider muiTheme={customTheme}>{storyFn()}</MuiThemeProvider>
+);
+addDecorator(muiDecorator);
 
 storiesOf('ImageAltTextPopover', module).add('ImageAltTextPopover', () => (
-  <MuiThemeProvider muiTheme={customTheme}>
-    <ImageAltTextPopover
-      open={true}
-      imageSrc="https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg"
-    />
-  </MuiThemeProvider>
+  <ImageAltTextPopover
+    open={true}
+    imageSrc="https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg"
+  />
 ));
 
 storiesOf('ImagePanel', module).add('ImagePanel', () => (
-  <MuiThemeProvider muiTheme={customTheme}>
-    <ImagePanel
-      open={true}
-      images={[
-        'https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg',
-        'http://www.mybligr.com/wp-content/uploads/2017/03/cute-and-lovable-pictures-of-rabbits-33.jpg',
-        'https://wallpaperclicker.com/storage/wallpaper/cute-cat-62650236.jpg'
-      ]}
-    />
-  </MuiThemeProvider>
+  <ImagePanel
+    open={true}
+    images={[
+      'https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg',
+      'http://www.mybligr.com/wp-content/uploads/2017/03/cute-and-lovable-pictures-of-rabbits-33.jpg',
+      'https://wallpaperclicker.com/storage/wallpaper/cute-cat-62650236.jpg'
+    ]}
+  />
 ));
