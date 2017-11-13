e797ac27af18248a2d05dbdd241a9880dde69193
Hemant Kumar <hemant@agilemedialab.in>
2017-11-13 13:50:25 +0000

    title story and module

--- jseditor/src/stories/index.js
+++ jseditor/src/stories/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import ImageAltTextPopover from '../components/Editor/Escribir/ImageAltTextPopover';
+import Title from '../components/Editor/Escribir/Title';
 
 storiesOf('ImageAltTextPopover', module).add('ImageAltTextPopover', () => (
-  <ImageAltTextPopover
-    open={true}
-    imageSrc="https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg"
-  />
+  <MuiThemeProvider>
+    <ImageAltTextPopover
+      open={true}
+      imageSrc="https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg"
+    />
+  </MuiThemeProvider>
 ));
+
+storiesOf('Title', module)
+  .add('default', () => (
+    <MuiThemeProvider>
+      <Title title="" />
+    </MuiThemeProvider>
+  ))
+  .add('with data', () => (
+    <MuiThemeProvider>
+      <Title title="Samsung Galaxy S8+, analysis: el major telefono del mercado es imperfecto, pero irresistible" />
+    </MuiThemeProvider>
+  ));
