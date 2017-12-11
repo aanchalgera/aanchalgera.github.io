e54c0abe41ba63c49258a4333c87af817d987211
Aanchal Sharma <aanchal1103@gmail.com>
2017-12-11 08:22:56 +0000

    package-lock.json changes

--- jseditor/src/stores/ConfigureStore.prod.js
+++ jseditor/src/stores/ConfigureStore.prod.js
@@ -4,13 +4,6 @@ import rootReducer from '../reducers';
 
 const middlewares = [thunk];
 
-/*
-if (process.env.NODE_ENV === 'development') {
-  const createLogger = require('redux-logger');
-  const logger = createLogger();
-  middlewares.push(logger);
-}
-*/
 const store = compose(applyMiddleware(...middlewares))(createStore)(
   rootReducer
 );
