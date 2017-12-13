e80fec1b760daf12248f9ab6291d8744b446f6c4
nishubindal <nishubindal89@gmail.com>
2017-12-13 18:28:55 +0530

    Added image toolbar

--- jseditor/src/components/Editor/Escribir/Image.js
+++ jseditor/src/components/Editor/Escribir/Image.js
@@ -12,22 +12,45 @@ type Props = {
   deleteSection: (index: number) => void
 };
 
-const Image = (props: Props) => {
-  const handleDelete = () => {
-    props.deleteSection(props.index);
+type State = {
+  openImageToolbar: boolean,
+  imageEl: SyntheticEvent<HTMLImageElement>
+};
+
+class Image extends React.PureComponent<Props, State> {
+  state = {
+    openImageToolbar: false,
+    imageEl: {}
   };
 
-  const handleEdit = () => {};
+  handleDelete = () => {
+    this.props.deleteSection(this.props.index);
+  };
 
-  const { alt, src, extension } = props;
-  const url = `${src}/original.${extension}`;
-  return (
-    <React.Fragment>
-      <img src={url} alt={alt} />
-      <ImageToolbar handleEdit={handleEdit} handleDelete={handleDelete} />
-    </React.Fragment>
-  );
-};
+  openImageToolbar = (event: SyntheticEvent<HTMLImageElement>) => {
+    this.setState({ openImageToolbar: true, imageEl: event.currentTarget });
+  };
+
+  closeImageToolbar = () => {
+    this.setState({ openImageToolbar: false });
+  };
+
+  render() {
+    const { alt, src, extension } = this.props;
+    const url = `${src}/original.${extension}`;
+    return (
+      <React.Fragment>
+        <img src={url} alt={alt} onClick={this.openImageToolbar} />
+        <ImageToolbar
+          handleDelete={this.handleDelete}
+          open={this.state.openImageToolbar}
+          imageEl={this.state.imageEl}
+          closeImageToolbar={this.closeImageToolbar}
+        />
+      </React.Fragment>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {};
