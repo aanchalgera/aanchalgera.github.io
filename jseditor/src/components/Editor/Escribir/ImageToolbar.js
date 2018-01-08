32b92a7708c145f8455511b5eca96b6ed82799e2
nishubindal <nishubindal89@gmail.com>
2018-01-08 12:06:46 +0530

    Fixed svg icon issue

--- jseditor/src/components/Editor/Escribir/ImageToolbar.js
+++ jseditor/src/components/Editor/Escribir/ImageToolbar.js
@@ -4,10 +4,15 @@ import { connect } from 'react-redux';
 import { ToolbarSeparator } from 'material-ui/Toolbar';
 import { deleteSection, openImagePanel, changeLayout } from 'actions/post';
 import { ActionDelete, EditorModeEdit } from 'material-ui/svg-icons';
-import ReactSVG from 'react-svg';
 import { ToolbarIcon } from '.';
-import { grey600, grey500, red500, grey700 } from 'material-ui/styles/colors';
-import SvgIcon from 'material-ui/SvgIcon';
+import { grey600, grey500, red500 } from 'material-ui/styles/colors';
+import {
+  smallLeftIcon,
+  smallCenterIcon,
+  smallRightIcon,
+  normalIcon,
+  bigIcon
+} from './lib/svgIcons';
 
 type Props = {
   handleDelete: () => void,
@@ -20,11 +25,6 @@ type Props = {
   selectedKey: string,
   maxId: number
 };
-
-// const iconStyles = {
-//   width: 28,
-//   height: 16,
-// };
 
 const ImageToolbar = (props: Props) => {
   const handleDelete = () => {
@@ -47,65 +47,6 @@ const ImageToolbar = (props: Props) => {
     return '';
   };
 
-  const smallCenterIcon = () => {
-    return (
-      <SvgIcon viewBox="0 0 18 16" color={grey700}>
-        <path
-          d="m0.000034,16.000038l18,0l0,-1.77778l-18,0l0,1.77778l0,0zm4,-3.55444l10,0l0,-8.89l-10,0l0,8.89l0,0zm-4,-12.44556l0,1.77778l18,0l0,-1.77778l-18,0l0,0z"
-          id="icon-column-small-a"
-        />
-      </SvgIcon>
-    );
-  };
-
-  const smallLeftIcon = () => {
-    return (
-      <SvgIcon viewBox="0 0 28 16" color={grey700}>
-        <path
-          d="m5.000023,16.000037l18,0l0,-1.77778l-18,0l0,1.77778l0,0zm7,-12l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm0,3l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm0,3l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm-12,2.44556l10,0l0,-8.89l-10,0l0,8.89l0,0zm5,-12.44556l0,1.77778l18,0l0,-1.77778l-18,0l0,0z"
-          id="icon-column-small-left-a"
-        />
-      </SvgIcon>
-    );
-  };
-
-  const smallRightIcon = () => {
-    return (
-      <SvgIcon viewBox="0 0 24 16" color={grey700}>
-        <path
-          d="m5.000023,16.000037l18,0l0,-1.77778l-18,0l0,1.77778l0,0zm7,-12l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm0,3l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm0,3l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm-12,2.44556l10,0l0,-8.89l-10,0l0,8.89l0,0zm5,-12.44556l0,1.77778l18,0l0,-1.77778l-18,0l0,0z"
-          id="icon-column-small-left-a"
-        />
-      </SvgIcon>
-    );
-  };
-
-  const normalIcon = () => {
-    return (
-      <SvgIcon
-        viewBox="0 0 18 16"
-        color={grey700}
-        style={{ height: '18px', width: '16px' }}
-      >
-        <path
-          d="m0,16.000002l18,0l0,-1.77778l-18,0l0,1.77778l0,0zm0,-3.55444l18,0l0,-8.89l-18,0l0,8.89l0,0zm0,-12.44556l0,1.77778l18,0l0,-1.77778l-18,0l0,0z"
-          id="icon-column-normal-a"
-        />
-      </SvgIcon>
-    );
-  };
-
-  const bigIcon = () => {
-    return (
-      <SvgIcon viewBox="0 0 35 16" color={grey700}>
-        <path
-          d="m9.000003,16l18,0l0,-1.77778l-18,0l0,1.77778l0,0zm-9,-3.55444l35,0l0,-8.89l-35,0l0,8.89l0,0zm9,-12.44556l0,1.77778l18,0l0,-1.77778l-18,0l0,0z"
-          id="icon-column-edge-a"
-        />
-      </SvgIcon>
-    );
-  };
-
   return (
     <React.Fragment>
       <ToolbarIcon
@@ -121,7 +62,7 @@ const ImageToolbar = (props: Props) => {
         tooltip="Pequeño, centrado"
       />
       <ToolbarIcon
-        ActionIcon={<ReactSVG path="/svgs/smallRight.svg" />}
+        ActionIcon={smallRightIcon()}
         handleClick={() => changeLayout('small', 'right')}
         className={getClassName('small', 'right')}
         tooltip="Pequeño, a la derecha"
@@ -138,12 +79,6 @@ const ImageToolbar = (props: Props) => {
         className={getClassName('large')}
         tooltip="Grande"
       />
-      {/*  <ToolbarIcon
-        ActionIcon={<ReactSVG path="/svgs/edge.svg" />}
-        handleClick={() => changeLayout('cover')}
-        className={getClassName('cover')}
-        tooltip="Anchura completa"
-      />*/}
       <ToolbarSeparator style={{ backgroundColor: grey500 }} key="seprator" />
       <ToolbarIcon
         ActionIcon={<EditorModeEdit color={grey600} />}
---
+++ jseditor/src/components/Editor/Escribir/lib/svgIcons.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import SvgIcon from 'material-ui/SvgIcon';
+import { grey700 } from 'material-ui/styles/colors';
+
+export const smallCenterIcon = () => {
+  return (
+    <SvgIcon color={grey700}>
+      <path d="m3,20l18,0l0,-1.77778l-18,0l0,1.77778l0,0zm4,-3.55444l10,0l0,-8.89l-10,0l0,8.89l0,0zm-4,-12.44556l0,1.77778l18,0l0,-1.77778l-18,0l0,0z" />
+    </SvgIcon>
+  );
+};
+
+export const smallLeftIcon = () => {
+  return (
+    <SvgIcon color={grey700}>
+      <path d="m5,20l18,0l0,-1.77778l-18,0l0,1.77778l0,0zm7,-12l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm0,3l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm0,3l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm-12,2.44556l10,0l0,-8.89l-10,0l0,8.89l0,0zm5,-12.44556l0,1.77778l18,0l0,-1.77778l-18,0l0,0z" />
+    </SvgIcon>
+  );
+};
+
+export const smallRightIcon = () => {
+  return (
+    <SvgIcon color={grey700}>
+      <path
+        d="m3,20l18,0l0,-1.77778l-18,0l0,1.77778l0,0zm7,-12l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm0,3l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm0,3l0,1.77778l16,0l0,-1.77778l-16,0l0,0zm-12,2.44556l10,0l0,-8.89l-10,0l0,8.89l0,0zm5,-12.44556l0,1.77778l18,0l0,-1.77778l-18,0l0,0z"
+        id="icon-column-small-right-a"
+        transform="rotate(-180 12,12)"
+      />
+    </SvgIcon>
+  );
+};
+
+export const normalIcon = () => {
+  return (
+    <SvgIcon color={grey700}>
+      <path d="m3,20l18,0l0,-1.77778l-18,0l0,1.77778l0,0zm0,-3.55444l18,0l0,-8.89l-18,0l0,8.89l0,0zm0,-12.44556l0,1.77778l18,0l0,-1.77778l-18,0l0,0z" />
+    </SvgIcon>
+  );
+};
+
+export const bigIcon = () => {
+  return (
+    <SvgIcon color={grey700}>
+      <path d="m2.90978,20.017716l18.18045,0l0,-1.78171l-18.18046,0l0,1.78171l0,0l0.00001,0zm-3.03008,-3.56231l24.2406,0l0,-8.90968l-24.2406,0l0,8.90968l0,0zm3.03008,-12.47312l0,1.78172l18.18045,0l0,-1.78172l-18.18046,0l0,0l0.00001,0z" />
+    </SvgIcon>
+  );
+};
