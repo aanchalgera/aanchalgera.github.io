c402427b082a601d859fa1971148f3d52d62a6ce
nishubindal <nishubindal89@gmail.com>
2018-01-08 23:39:10 +0530

    Added middleware for body content

--- API/server.js
+++ API/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 require('babel-register');
+var bodyParser = require('body-parser');
 
 let env = process.env.NODE_ENV || 'development';
 global.envConfig = require(`./Config/config.${env}.json`);
@@ -8,6 +9,8 @@ global.envConfig = require(`./Config/config.${env}.json`);
 let app = require('express')();
 let routes = require('./Router');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use('/api', routes);
 
 app.listen(81,"0.0.0.0", () => 
