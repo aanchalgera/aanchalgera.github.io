e414b358dec0cb624c52c91cf6e0848921211ab9
nishubindal <nishubindal89@gmail.com>
2018-01-17 10:16:38 +0530

    template changes for news

--- jseditor/src/components/Editor/Escribir/lib/newsPages.js
+++ jseditor/src/components/Editor/Escribir/lib/newsPages.js
@@ -9,20 +9,20 @@ const page1 = (
         <span className="news-title">Lo nuevo en Alfa (7)</span>
         <h2 className="type-title-dark">Lo esencial: texto e imágenes</h2>
         <p className="paragraph">
-          En Alfa la principal novedad es que nos quitamos de en medio el
-          código visible. A partir de ahora el formato que des a los textos e
-          imágenes se aplicará directamente sobre ellos.
+          En Alfa la principal novedad es que nos quitamos de en medio el código
+          visible. A partir de ahora el formato que des a los textos e imágenes
+          se aplicará directamente sobre ellos.
         </p>
         <p className="paragraph">
-          No obstante, no es una preview 100% real, para eso times el{' '}
-          {'"ojo"'} en la esquina superior derecha
+          No obstante, no es una preview 100% real, para eso times el {'"ojo"'}{' '}
+          en la esquina superior derecha
         </p>
         <h4>Texto con formato</h4>
         <ul>
           <li>- Abre el editor y escribe directamente.</li>
           <li>
-            - Para dar formato al texto que acabas de escribir, resáltalo y
-            las opciones aparecerán en pantalla.
+            - Para dar formato al texto que acabas de escribir, resáltalo y las
+            opciones aparecerán en pantalla.
           </li>
           <li>- También funcionan los atajos de Markdown.</li>
         </ul>
