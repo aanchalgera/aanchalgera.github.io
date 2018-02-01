d04c80d5499be84d095caa32fe63c9ea99692ce8
Aanchal Sharma <aanchal1103@gmail.com>
2018-02-01 05:18:16 +0000

    Added missing export

--- jseditor/src/components/Editor/ImageUploader/index.jsx
+++ jseditor/src/components/Editor/ImageUploader/index.jsx
@@ -3,6 +3,7 @@ export { ImageAltTextPopover } from './ImageAltTextPopover';
 export { CloseButton } from './CloseButton';
 export { Thumbnail } from './Thumbnail';
 export { Label } from './Label';
+export { DialogTitle } from './DialogTitle';
 export { S3Uploader } from './S3Uploader';
 export { UrlUploader } from './UrlUploader';
 export { default as ImageUploader } from './ImageUploader';
